00ad307249ecf3de0b28fbabfc39e65bca12c1c4
matheus.silverio <matheus.silverio@hydrasystems.com.br>
2022-11-21 22:02:14 -0300

    fix: run app

--- Client-app/app/src/main/java/com/example/client_app/Config.java
+++ Client-app/app/src/main/java/com/example/client_app/Config.java
@@ -1,6 +1,6 @@
 package com.example.client_app;
 
 public class Config {
-    public static String urlApi = "https://rickandmortyapi.com/api/character?page=1";
-    public static String linkImagem = "https://rickandmortyapi.com/api/avatar/";
+    public static String urlApi = "https://rickandmortyapi.com/api/character?page=";
+    public static String linkImagem = "https://rickandmortyapi.com/api/avatar";
 }
--- Client-app/app/src/main/java/com/example/client_app/DetalhesActivity.java
+++ Client-app/app/src/main/java/com/example/client_app/DetalhesActivity.java
@@ -2,13 +2,38 @@ package com.example.client_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.client_app.datasources.DownloadImagem;
 
 public class DetalhesActivity extends AppCompatActivity {
+
+    TextView nome;
+    ImageView imagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes);
+
+        nome = findViewById(R.id.nome);
+        imagem = findViewById(R.id.imagem);
+
+        //capturando o caminho de tela utilizado para abrir esta tela
+        Intent dadosRecebidos = getIntent();
+        if (dadosRecebidos != null) {
+            //capturando os dados recebidos no caminho de tela
+            Bundle params = dadosRecebidos.getExtras();
+            if (params != null) {
+                //adicionando o nome do pokemon no texto da tela
+                nome.setText(params.getString("name"));
+
+                //carregando a imagem da web no objeto imagem da tela
+                new DownloadImagem(imagem).execute(params.getString("imagem"));
+            }
+        }
     }
 }
